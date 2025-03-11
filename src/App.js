d6bac7527033a94fdb3a699072da5021a6e73fad
Barrett Anderson <barrett1614@gmail.com>
2025-03-11 15:01:21 -0500

    Initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BubblyButton from "./components/bubbly-button/Bubbly-Button";
+
+const App = () => {
+  return (
+    <div>
+      <BubblyButton>Click Me</BubblyButton>
+    </div>
+  );
+};
+
+export default App;
