e80d3277f3f8364e713a7b01602c677659ea5385
Barrett Anderson <barrett1614@gmail.com>
2025-04-30 22:07:04 -0500

    JSX in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import BubblyButton from "./components/bubbly-button/Bubbly-Button";
 import HeartyButton from "./components/hearty-button/Hearty-Button";
 import ExpandingButton from "./components/ExpandingButton/ExpandingButton";
+import GlowingButton from "./components/GlowingButton/GlowingButton";
 
 const App = () => {
   return (
@@ -14,6 +15,9 @@ const App = () => {
       </div>
       <div>
         <HeartyButton />
+      </div>
+      <div>
+        <GlowingButton />
       </div>
     </>
   );
