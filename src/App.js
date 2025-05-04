cc794a4a3cd93dd2b1d676556cd2bae90f8103c6
Barrett Anderson <barrett1614@gmail.com>
2025-05-04 18:16:14 -0500

    Slowed dark-mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+
+import ThemeToggleButton from "./components/ThemeToggleButton/ThemeToggleButton.jsx";
 import BubblyButton from "./components/bubbly-button/Bubbly-Button";
 import HeartyButton from "./components/hearty-button/Hearty-Button";
 import ExpandingButton from "./components/ExpandingButton/ExpandingButton";
 import GlowingButton from "./components/GlowingButton/GlowingButton";
 import GradientSlideButton from "./components/GradientSlideButton/GradientSlideButton";
 import LoadingSuccessButton from "./components/LoadingSuccessButton/LoadingSuccessButton";
-import ThemeToggleButton from "./components/ThemeToggleButton/ThemeToggleButton.jsx";
 
 const App = () => {
   return (
     <>
+      <div style={{ padding: "2rem", textAlign: "center" }}>
+        <ThemeToggleButton />
+      </div>
       <div>
         <ExpandingButton>Click Me!</ExpandingButton>
       </div>
@@ -26,15 +30,10 @@ const App = () => {
         <GradientSlideButton />
       </div>
       <div style={{ padding: "2rem", textAlign: "center" }}>
-        <h2>Async Button Feedback</h2>
         <LoadingSuccessButton
           label="Submit Form"
           onClick={() => console.log("Form submitted")}
         />
-      </div>
-      <div style={{ padding: "2rem", textAlign: "center" }}>
-        <h2>Toggle Light / Dark Mode</h2>
-        <ThemeToggleButton />
       </div>
     </>
   );
